72d939bf306bb866b8e16086edf32c3d31b8992b
OscarJMatosCanul <13470202@itcampeche.edu.mx>
2017-10-12 18:26:01 -0500

    Tercera version: Ajuste del codigo para evitar se pierdan datos cuando se gira o pausa la App.

--- app/src/main/java/com/oscarmatos/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/oscarmatos/android/contadorapp/MainActivity.java
@@ -8,27 +8,35 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String Key_Conteo ="conteo";
+    private int conteo;
     EditText mCampoContador;
     Button mBotonContar;
     Button mBotonLimpiar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCampoContador=(EditText) findViewById(R.id.campo_Conteo);
+        mCampoContador=(EditText)findViewById(R.id.campo_Conteo);
         mBotonContar=(Button)findViewById(R.id.boton_Contar);
         mBotonLimpiar=(Button)findViewById(R.id.boton_Reiniciar);
+
+        if(savedInstanceState==null) {
+            conteo = 0;
+        }else{
+            conteo=savedInstanceState.getInt(Key_Conteo);
+        }
+
+        mCampoContador.setText(Integer.toString(conteo));
 
         mBotonContar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-             String s = mCampoContador.getText().toString();
-                double cont = Double.parseDouble(s);
-                cont++;
-                s=String.valueOf(cont);
-                mCampoContador.setText(s);
+                conteo++;
+                mCampoContador.setText(Integer.toString(conteo));
             }
         });
 
@@ -36,7 +44,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mCampoContador.setText("0.0");
+                conteo=0;
             }
     });
     }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Key_Conteo,conteo);
+    }
+
 }
